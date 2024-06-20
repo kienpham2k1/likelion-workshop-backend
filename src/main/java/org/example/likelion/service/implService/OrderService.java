9d014cfc02605cc7e59bcf3cb2b65d66494de214
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-06-20 11:49:45 +0700

    add service

---
+++ src/main/java/org/example/likelion/service/implService/OrderService.java
@@ -0,0 +1,11 @@
+package org.example.likelion.service.implService;
+
+import org.example.likelion.model.Order;
+import org.example.likelion.service.IOrderService;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService implements IOrderService {
+}
---
+++ src/main/java/org/example/likelion/service/implService/VoucherService.java
@@ -0,0 +1,11 @@
+package org.example.likelion.service.implService;
+
+import org.example.likelion.service.IProductService;
+import org.example.likelion.service.IVoucherService;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VoucherService implements IVoucherService {
+}
