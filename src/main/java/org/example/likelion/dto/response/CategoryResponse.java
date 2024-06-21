0b0e1f877c6878f00164453f49b110ad68e25ecb
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-06-21 10:06:52 +0700

    Category feat

---
+++ src/main/java/org/example/likelion/dto/response/CategoryResponse.java
@@ -0,0 +1,13 @@
+package org.example.likelion.dto.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryResponse {
+     private String id;
+     private String name;
+}
--- src/main/java/org/example/likelion/repository/CategoryRepository.java
+++ src/main/java/org/example/likelion/repository/CategoryRepository.java
@@ -1,10 +1,12 @@
 package org.example.likelion.repository;
 
 import org.example.likelion.model.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
+    Page<Category> findByNameEqualsIgnoreCase(String name, Pageable pageable);
 }
