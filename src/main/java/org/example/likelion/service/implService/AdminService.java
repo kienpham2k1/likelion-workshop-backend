f91cd0f27c6bda34e22291a3a720bdd3de5664ab
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-06-23 01:53:53 +0700

    add service base

---
+++ src/main/java/org/example/likelion/service/implService/AdminService.java
@@ -0,0 +1,6 @@
+package org.example.likelion.service.implService;
+
+import org.example.likelion.service.IAdminService;
+
+public class AdminService implements IAdminService {
+}
---
+++ src/main/java/org/example/likelion/service/implService/UserService.java
@@ -0,0 +1,6 @@
+package org.example.likelion.service.implService;
+
+import org.example.likelion.service.IUserService;
+
+public class UserService implements IUserService {
+}
