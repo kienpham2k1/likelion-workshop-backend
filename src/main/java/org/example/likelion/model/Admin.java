59ac366f2b7a0105870cd03070bd9c5d63889570
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-06-23 01:51:09 +0700

    add model

---
+++ src/main/java/org/example/likelion/model/Admin.java
@@ -0,0 +1,31 @@
+package org.example.likelion.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name = "admin")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "admin_id")
+    private String id;
+    @Column(unique = true)
+    @NotBlank
+    private String username;
+    @NotNull
+    @NotBlank
+    private String password;
+    @Column(name = "role_id", nullable = false)
+    private String roleId;
+    @ManyToOne
+    @JoinColumn(name = "role_id", referencedColumnName = "role_id", insertable = false, updatable = false)
+    private Role role;
+}
