880ca2ae23e37c5b314364142557c42ac5db095d
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-06-24 15:39:11 +0700

    fix cors network

---
+++ src/main/java/org/example/likelion/config/WebConfig.java
@@ -0,0 +1,48 @@
+package org.example.likelion.config;
+
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowedOrigins("*");
+    }
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializerByType(Date.class, new DateSerializer(
+                false, new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")));
+        builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(
+                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")));
+        converters.add(new ByteArrayHttpMessageConverter());
+        converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
+    }
+}
