9cb3eb2efc245c6f1012b0367d1fb77d48ce9983
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-07-03 21:53:01 +0700

    init models

---
+++ src/main/java/org/example/likelion/model/Members.java
@@ -0,0 +1,24 @@
+package org.example.likelion.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "members")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Members {
+    @Id
+    private String id;
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
