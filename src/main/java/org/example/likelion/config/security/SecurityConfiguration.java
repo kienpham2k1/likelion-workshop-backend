e1dbc4ba6b376c88fc9e0ec8807b5e1c9ae79c5d
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-07-03 21:54:32 +0700

    add sample controller

--- src/main/java/org/example/likelion/config/security/SecurityConfiguration.java
+++ src/main/java/org/example/likelion/config/security/SecurityConfiguration.java
@@ -51,7 +51,8 @@ public class SecurityConfiguration {
             "/swagger-ui.html",
             "/api/v1/file/upload/**",
             "/api/v1/ai-recommendation/**",
-            "/api/v1/admin/**"
+            "/api/v1/admin/**",
+            "/ws/**"
     };
 
     @Bean
