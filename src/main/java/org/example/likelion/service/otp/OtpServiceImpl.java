52b163420c5437d01738bdd9e2aae2b773fb0c51
Tzuy284 <93607267+Tzuy284@users.noreply.github.com>
2024-07-05 15:36:44 +0700

    style: clean code

--- src/main/java/org/example/likelion/service/otp/OtpServiceImpl.java
+++ src/main/java/org/example/likelion/service/otp/OtpServiceImpl.java
@@ -4,17 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.example.likelion.constant.InfoMessage;
-import org.example.likelion.dto.auth.UserDetailsImpl;
 import org.example.likelion.dto.mapper.IUserMapper;
 import org.example.likelion.dto.response.UserResponse;
 import org.example.likelion.model.User;
 import org.example.likelion.repository.UserRepository;
+import org.example.likelion.service.auth.AuthenticationService;
 import org.example.likelion.service.mail.MailService;
 import org.example.likelion.service.sms.InfobipService;
 import org.example.likelion.utils.OtpGeneratorUtils;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -27,66 +25,54 @@ public class OtpServiceImpl implements OtpService {
     private final InfobipService infobipService;
     private final MailService mailService;
     private final UserRepository userRepository;
+    private final AuthenticationService authenticationService;
 
     @Override
     public Boolean sendOtpVisSms() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            Object principal = authentication.getPrincipal();
-            if (principal instanceof UserDetailsImpl userDetails) {
-                Optional<User> userInfo = userRepository.findByUsername(userDetails.getUsername());
+        UserResponse userDetails = authenticationService.getCurrentUserInfo().orElse(null);
+        Optional<User> userInfo = userRepository.findByUsername(userDetails.getUsername());
 
-                UserResponse userResponse = IUserMapper.INSTANCE.toDtoRegisterResponse(userInfo.orElse(null));
-                boolean rs;
-                Integer otpValue = otpGenerator.generateOTP(userResponse.getUsername());
-                if (otpValue == -1) {
-                    return false;
-                }
-                HttpStatusCode returnStatus = infobipService.sendOtpCode("+84" + userResponse.getPhoneNumber(), InfoMessage.OTP_MESSAGE + otpValue);
-                rs = returnStatus.is2xxSuccessful();
-                return rs;
-            }
+        UserResponse userResponse = IUserMapper.INSTANCE.toDtoRegisterResponse(userInfo.orElse(null));
+        boolean rs;
+        Integer otpValue = otpGenerator.generateOTP(userResponse.getUsername());
+        if (otpValue == -1) {
+            return false;
         }
-        return false;
+        HttpStatusCode returnStatus = infobipService.sendOtpCode("+84" + userResponse.getPhoneNumber(), InfoMessage.OTP_MESSAGE + otpValue);
+        rs = returnStatus.is2xxSuccessful();
+        return rs;
+
     }
 
     @SneakyThrows
     @Override
     public Boolean sendOtpVisEmail() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            Object principal = authentication.getPrincipal();
-            if (principal instanceof UserDetailsImpl userDetails) {
-                Optional<User> userInfo = userRepository.findByUsername(userDetails.getUsername());
+        UserResponse userDetails = authenticationService.getCurrentUserInfo().orElse(null);
 
-                UserResponse userResponse = IUserMapper.INSTANCE.toDtoRegisterResponse(userInfo.orElse(null));
-                boolean rs;
-                Integer otpValue = otpGenerator.generateOTP(userResponse.getUsername());
-                if (otpValue == -1) {
-                    return false;
-                }
-                mailService.sendOtpCode(userResponse.getEmail(), otpValue.toString(), userResponse.getUsername());
-                return true;
-            }
+        Optional<User> userInfo = userRepository.findByUsername(userDetails.getUsername());
+
+        UserResponse userResponse = IUserMapper.INSTANCE.toDtoRegisterResponse(userInfo.orElse(null));
+        boolean rs;
+        Integer otpValue = otpGenerator.generateOTP(userResponse.getUsername());
+        if (otpValue == -1) {
+            return false;
         }
-        return false;
+        mailService.sendOtpCode(userResponse.getEmail(), otpValue.toString(), userResponse.getUsername());
+        return true;
+
     }
 
     @Override
     public Boolean validateOTP(Integer otpNumber) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated()) {
-            Object principal = authentication.getPrincipal();
-            if (principal instanceof UserDetailsImpl userDetails) {
-                Integer cacheOTP = otpGenerator.getOPTByKey(userDetails.getUsername());
-                if (cacheOTP != null && cacheOTP.equals(otpNumber)) {
-                    otpGenerator.clearOTPFromCache(userDetails.getUsername());
-                    User userInfo = userRepository.findUserByUsername(userDetails.getUsername());
-                    userInfo.setVerify(true);
-                    userRepository.save(userInfo);
-                    return true;
-                }
-            }
+        UserResponse userDetails = authenticationService.getCurrentUserInfo().orElse(null);
+
+        Integer cacheOTP = otpGenerator.getOPTByKey(userDetails.getUsername());
+        if (cacheOTP != null && cacheOTP.equals(otpNumber)) {
+            otpGenerator.clearOTPFromCache(userDetails.getUsername());
+            User userInfo = userRepository.findUserByUsername(userDetails.getUsername());
+            userInfo.setVerify(true);
+            userRepository.save(userInfo);
+            return true;
         }
         return false;
     }
