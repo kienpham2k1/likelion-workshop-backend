08dac86c2e0e8ec16e9613a5536489ed09f1c6aa
Tzuy284 <93607267+Tzuy284@users.noreply.github.com>
2024-07-11 16:48:23 +0700

    fix: promotion and add payment method class

--- src/main/java/org/example/likelion/dto/response/CategoryResponse.java
+++ src/main/java/org/example/likelion/dto/response/CategoryResponse.java
@@ -2,6 +2,8 @@ package org.example.likelion.dto.response;
 
 import lombok.*;
 
+import java.util.Set;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -10,5 +12,5 @@ import lombok.*;
 public class CategoryResponse {
     private String id;
     private String name;
-    private PromotionResponse promotion;
+    private Set<PromotionResponse> promotions;
 }
