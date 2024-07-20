925e1efabf35bf776c8c7f9a3d73a3151a37b0d2
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-07-20 18:15:14 +0700

    remove code

--- src/main/java/org/example/likelion/repository/CategoryRepository.java
+++ src/main/java/org/example/likelion/repository/CategoryRepository.java
@@ -6,10 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
-//    @Query("select c from Category c where :name is null or upper(cast(c.name as string)) like upper(concat('%', cast(:name as string), '%'))")
-//    Page<Category> findByNameIsContainingIgnoreCase(@Param("name") String name, Pageable pageable);
-//
-//    @Query("SELECT c FROM Category c join c.promotions p " +
-//            "where p.id = :promotionId")
-//    List<Category> findCategoriesByPromotionId(@Param("promotionId") String promotionId);
 }
