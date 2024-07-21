cb64e98da2cf4b23002fea18d38889f31d0c4896
Pham Thanh Kien <phamthanhkien2k1@gmail.com>
2024-07-21 17:17:06 +0700

    modifier query

--- src/main/java/org/example/likelion/repository/CategoryRepository.java
+++ src/main/java/org/example/likelion/repository/CategoryRepository.java
@@ -2,8 +2,15 @@ package org.example.likelion.repository;
 
 import org.example.likelion.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
+    @Override
+    @Query("select c from Category c JOIN FETCH c.promotions p  where c.id = :categoryId and p.expiredDate > current_date ")
+    Optional<Category> findById(@Param("categoryId") String id);
 }
