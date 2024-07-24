42a214a4a7c98ba6ec756ead846bc6e46b98478d
Tzuy284 <93607267+Tzuy284@users.noreply.github.com>
2024-07-24 08:58:49 +0700

    Feat/redis ai search (#65)
    * feat: add some rule for bot
    
    * feat: add some rule for bot
    
    * feat: redis
    
    * fixL cloud sonar

--- src/main/java/org/example/likelion/service/AIService.java
+++ src/main/java/org/example/likelion/service/AIService.java
@@ -2,11 +2,10 @@ package org.example.likelion.service;
 
 import org.example.likelion.dto.chatGPT.GeminiAIRequest;
 import org.example.likelion.dto.chatGPT.dto.GeminiAIResponseDTO;
-import org.example.likelion.dto.request.AIRecommendationRequest;
 import org.example.likelion.dto.response.AIRecommendationResponse;
 
 public interface AIService {
-    AIRecommendationResponse getRecommendation(AIRecommendationRequest request);
+    AIRecommendationResponse getRecommendation(String request);
 
     GeminiAIResponseDTO getRecommendation(GeminiAIRequest geminiAIRequest);
 
